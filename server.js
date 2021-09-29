1e60eeaad3799922ef6c03fe3d75a3bd05b67b22
Aboubacar Sidikou <a.asidikou0709@gmail.com>
2021-09-29 13:27:43 +0100

    add login POST route

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const mockUsersData = [
     { name: 'Mark' },
     { name: 'Jill' }
@@ -22,6 +24,27 @@ app.get('/users/:username', (req, res) => {
         message: 'Got one user.',
         user: req.params.username,
     });
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const mockUsername = "billyTheKid";
+    const mockPassword = "abcdef";
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            success: true,
+            message: 'password and username match',
+            token: 'token',
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match',
+        });
+    }
 });
 
 app.listen(8000, () => {
